9c5714e0c99890b2b7feed0a9de53c0613e311a8
Rajeev Kumar <work.rajeevkumar@gmail.com>
2025-06-13 06:41:23 +0530

    DailyWorks is ready to deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dailyworks-vl43.onrender.com",
+  baseURL: "https://dailyworks-vl43.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
